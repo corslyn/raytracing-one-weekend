9225c5440c76b7eb02d286170dd2db14236ba6ee
Enzo Manzinali <enzomanzinali@protonmail.com>
2025-10-21 21:43:48 +0200

    camera placement

--- src/camera.rs
+++ src/camera.rs
@@ -16,12 +16,18 @@ pub struct Camera {
     pub samples_per_pixel: i32,
     pub max_depth: i32,
     pub fov: f64,
+    pub lookfrom: Point3,
+    pub lookat: Point3,
+    pub vup: Vec3,
     image_height: i32,
     center: Point3,
     pixel00_loc: Point3,
     pixel_delta_u: Point3,
     pixel_delta_v: Point3,
     pixel_samples_scale: f64,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
 }
 
 impl Camera {
@@ -41,6 +47,12 @@ impl Camera {
             samples_per_pixel: 10,
             max_depth: 10,
             fov: 90.0,
+            lookfrom: Point3::new(0.0, 0.0, 0.0),
+            lookat: Point3::new(0.0, 0.0, -1.0),
+            vup: Vec3::new(0.0, 1.0, 0.0),
+            u: Vec3::new(0.0, 1.0, 0.0),
+            v: Vec3::new(0.0, 1.0, 0.0),
+            w: Vec3::new(0.0, 1.0, 0.0),
         }
     }
 
@@ -95,24 +107,27 @@ impl Camera {
 
         self.pixel_samples_scale = 1.0 / self.samples_per_pixel as f64;
 
-        self.center = Point3::new(0.0, 0.0, 0.0);
+        self.center = self.lookfrom;
 
-        let focal_length = 1.0;
+        let focal_length = (self.lookfrom - self.lookat).length();
         let theta = degrees_to_radians(self.fov);
         let h = (theta / 2.0).tan();
 
         let viewport_height = 2.0 * h * focal_length;
         let viewport_width = viewport_height * (self.image_width as f64 / self.image_height as f64);
-        self.center = Point3::new(0.0, 0.0, 0.0);
 
-        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+        self.w = unit_vector(self.lookfrom - self.lookat);
+        self.u = unit_vector(cross(self.vup, self.w));
+        self.v = cross(self.w, self.u);
+
+        let viewport_u = viewport_width * self.u;
+        let viewport_v = viewport_height * -self.v;
 
         self.pixel_delta_u = viewport_u / self.image_width as f64;
         self.pixel_delta_v = viewport_v / self.image_height as f64;
 
         let viewport_upper_left =
-            self.center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+            self.center - focal_length * self.w - viewport_u / 2.0 - viewport_v / 2.0;
 
         self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v);
     }
--- src/main.rs
+++ src/main.rs
@@ -31,60 +31,50 @@ fn main() {
 
     let r = core::f64::consts::FRAC_PI_4; // 90 deg
 
-    // let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
-    // let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    // let material_left = Dielectic::new(1.5);
-    // let material_bubble = Dielectic::new(1.0 / 1.5);
-    // let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
-
-    let material_left = Lambertian::new(Color::new(0.0, 0.0, 1.0));
-    let material_right = Lambertian::new(Color::new(1.0, 0.0, 0.0));
-    // world.add(Arc::new(Sphere::new(
-    //     Point3::new(0.0, -100.5, -1.0),
-    //     100.0,
-    //     Arc::new(material_ground),
-    // )));
-    //
-    // world.add(Arc::new(Sphere::new(
-    //     Point3::new(0.0, 0.0, -1.2),
-    //     0.5,
-    //     Arc::new(material_center),
-    // )));
-    //
-    // world.add(Arc::new(Sphere::new(
-    //     Point3::new(-1.0, 0.0, -1.0),
-    //     0.5,
-    //     Arc::new(material_left),
-    // )));
-    // world.add(Arc::new(Sphere::new(
-    //     Point3::new(-1.0, 0.0, -1.0),
-    //     0.4,
-    //     Arc::new(material_bubble),
-    // )));
-    //
-    // world.add(Arc::new(Sphere::new(
-    //     Point3::new(1.0, 0.0, -1.0),
-    //     0.5,
-    //     Arc::new(material_right),
-    // )));
+    let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
+    let material_left = Dielectic::new(1.5);
+    let material_bubble = Dielectic::new(1.0 / 1.5);
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
 
     world.add(Arc::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        Arc::new(material_ground),
+    )));
+
+    world.add(Arc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        Arc::new(material_center),
+    )));
+
+    world.add(Arc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         Arc::new(material_left),
     )));
+    world.add(Arc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        Arc::new(material_bubble),
+    )));
 
     world.add(Arc::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         Arc::new(material_right),
     )));
+
     let mut camera = Camera::new();
 
     camera.aspect_ratio = 16.0 / 9.0;
     camera.image_width = 400;
     camera.samples_per_pixel = 100;
     camera.max_depth = 50;
-    camera.fov = 90.0;
+    camera.fov = 20.0;
+    camera.lookfrom = Point3::new(-2.0, 2.0, 1.0);
+    camera.lookat = Point3::new(0.0, 0.0, -1.0);
+    camera.vup = Vec3::new(0.0, 1.0, 0.0);
     camera.render(&world);
 }
